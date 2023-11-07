3fd3b68d6f51396521ba5709d378db12d2af8288
Grant Heath <heathgr@gmail.com>
2023-11-06 18:16:00 -0700

    Added hsv to rgb conversion to lesson 16.

--- lesson16/main.py
+++ lesson16/main.py
@@ -3,6 +3,7 @@
 import RPi.GPIO as GPIO
 import ADC0834
 import time
+from utils import hsv_to_rgb
 
 GPIO.setmode(GPIO.BCM)
 ADC0834.setup()
@@ -12,6 +13,12 @@ try:
         sVal = ADC0834.getResult(1)
         lVal = ADC0834.getResult(2)
         print(f"h: {hVal} s: {sVal} l: {lVal}")
+        rgb = hsv_to_rgb(
+            hVal / 255,
+            sVal / 255,
+            lVal / 255
+        )
+        print(f"r: {rgb[0]} g: {rgb[1]} {rgb[2]}")
         time.sleep(.2)
 
 except KeyboardInterrupt:
