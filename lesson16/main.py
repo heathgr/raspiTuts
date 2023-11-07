dcf8b5e7328ffc7055aa30d484c040d5d7ce3edc
Grant Heath <heathgr@gmail.com>
2023-11-06 18:22:44 -0700

    Configured RGB led pwms.

--- lesson16/main.py
+++ lesson16/main.py
@@ -5,8 +5,24 @@ import ADC0834
 import time
 from utils import hsv_to_rgb
 
+LED_BLUE = 26
+LED_GREEN = 19
+LED_RED = 13
+
 GPIO.setmode(GPIO.BCM)
 ADC0834.setup()
+GPIO.setup(LED_BLUE, GPIO.OUT)
+GPIO.setup(LED_GREEN, GPIO.OUT)
+GPIO.setup(LED_RED, GPIO.OUT)
+
+rPwmAgent = GPIO.PWM(LED_RED, 2000)
+gPwmAgent = GPIO.PWM(LED_GREEN, 2000)
+bPwmAgent = GPIO.PWM(LED_BLUE, 2000)
+
+rPwmAgent.start(0)
+gPwmAgent.start(0)
+bPwmAgent.start(0)
+
 try:
     while True:
         hVal = ADC0834.getResult(0)
@@ -18,7 +34,10 @@ try:
             sVal / 255,
             lVal / 255
         )
-        print(f"r: {rgb[0]} g: {rgb[1]} {rgb[2]}")
+        print(f"r: {rgb[0]} g: {rgb[1]} b: {rgb[2]}")
+        rPwmAgent.ChangeDutyCylce(int(rgb[0] * 100))
+        gPwmAgent.ChangeDutyCylce(int(rgb[1] * 100))
+        bPwmAgent.ChangeDutyCylce(int(rgb[2] * 100))
         time.sleep(.2)
 
 except KeyboardInterrupt:
