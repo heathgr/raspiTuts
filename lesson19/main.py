a4a5dca2a05b47cd0bbbe7fbc11e6e727d5895f1
Grant Heath <heathgr@gmail.com>
2023-11-07 19:22:36 -0700

    Initial commit for lesson 19.

---
+++ lesson19/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import atexit
+from time import sleep
+
+SERVO = 13
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERVO, GPIO.OUT)
+
+servoPwm = GPIO.PWM(SERVO, 100)
+servoPwm.start(0)
+
+
+def cleanup():
+    GPIO.cleanup()
+    print('GPIO Good to Go')
+
+
+atexit.register(cleanup)
+
+while True:
+    value = float(input("PWM Value: "))
+    servoPwm.ChangeDutyCycle(value)
+    sleep(.1)
