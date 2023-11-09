3a1a93859ecd474915b2b6c0a597d174675d1272
Grant Heath <heathgr@gmail.com>
2023-11-08 19:18:57 -0700

    Updated servo test.

--- lesson19/zero-test.py
+++ lesson19/zero-test.py
@@ -3,7 +3,7 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(26, min_angle=90, max_angle=90)
+servo = AngularServo(26, min_angle=-90, max_angle=90)
 
 while True:
     value = float(input("Servo Angle: "))
