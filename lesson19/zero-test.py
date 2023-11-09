22598fe006e18c0168097a2cc718155946c289b2
Grant Heath <heathgr@gmail.com>
2023-11-09 19:56:05 +0000

    Use correct settings for servo test.

--- lesson19/zero-test.py
+++ lesson19/zero-test.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 from gpiozero import Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
-servo = Servo(26)
+factory = PiGPIOFactory()
+servo = Servo(26, pin_factory=factory, frame_width=0.02, min_pulse_width=0.0005, max_pulse_width=0.0025)
 
 while True:
     value = float(input("Servo Value: "))
     servo.value = value
-    sleep(2)
+    sleep(0.2)
