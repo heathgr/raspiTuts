61bc472b2eb44cb74ae870b8f637e144935bd93b
Grant Heath <heathgr@gmail.com>
2023-11-09 17:15:43 -0700

    Fix bug setting servo.

--- lesson20/main.py
+++ lesson20/main.py
@@ -11,6 +11,6 @@ servo = Servo(26, pin_factory=factory, frame_width=0.02,
 
 while True:
     value = (potentiometer.value * 2.0) - 1.0
-    servo.value(value)
+    servo.value = value
     print(f"value: {value}")
     sleep(0.2)
