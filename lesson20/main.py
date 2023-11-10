9ea3fc40b14c33682efce6532dc711d293fbedb2
Grant Heath <heathgr@gmail.com>
2023-11-09 17:10:44 -0700

    Add servo controlls.

--- lesson20/main.py
+++ lesson20/main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
-from gpiozero import MCP3008
+from gpiozero import MCP3008, Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
 potentiometer = MCP3008(0)
+servo = Servo(26, pin_factory=factory, frame_width=0.02,
+              min_pulse_width=0.0005, max_pulse_width=0.0025)
 
 while True:
-    print(f"potentiometer value: {potentiometer}")
+    value = (potentiometer.value * 2) - 1
+    servo.value(value)
+    print(f"value: {value}")
     sleep(0.2)
