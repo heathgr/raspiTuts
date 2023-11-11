d825aec4217f243447bedb122a835d3bf79cc4b6
Grant Heath <heathgr@gmail.com>
2023-11-11 11:15:22 -0700

    Initial input for lesson 21.

---
+++ lesson21/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from time import sleep, time
+from RPi import GPIO
+import atexit
+
+
+def cleanExit():
+    print("Cleaning GPIO")
+    GPIO.clean()
+
+
+atexit.register(cleanExit)
+
+TRIGGER = 26
+ECHO = 19
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIGGER, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+while True:
+    GPIO.output(TRIGGER, 0)
+    sleep(2E-6)
+    GPIO.output(TRIGGER, 1)
+    sleep(10E-6)
+    GPIO.output(TRIGGER, 0)
+    while GPIO.input(ECHO) == 0:
+        pass
+    echoStartTime = time()
+    while GPIO.input(ECHO) == 1:
+        pass
+    echoEndTime = time()
+    print(echoStartTime - echoEndTime)
