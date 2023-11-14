244730bd7a0de17c129a2c032b2b86305aee17fd
Grant Heath <heathgr@gmail.com>
2023-11-13 18:54:27 -0700

    Initial commit for lesson 25.

---
+++ lesson25/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from RPi import GPIO
+from time import sleep
+from dht11 import DHT11
+import atexit
+
+
+def cleanExit():
+    print("Cleaning up GPIO.")
+    GPIO.cleanup()
+
+
+atexit(cleanExit)
+
+SENSOR_PIN = 21
+
+sensor = DHT11(pin=SENSOR_PIN)
+
+while True:
+    value = sensor.read()
+    if value.is_valid():
+        print(f"Temp: {value.temperature} Humidity: {value.humidity}")
+    sleep(0.2)
