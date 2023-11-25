bfa67243282baac929b37e844a55f982926bf28e
Grant Heath <heathgr@gmail.com>
2023-11-24 22:35:00 -0700

    Adding a program for passive buzzer.

---
+++ lesson27b/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from time import sleep
+from RPi import GPIO
+import atexit
+
+BUZZER = 26
+
+
+def cleanExit():
+    print("Cleaning up GPIO.")
+    GPIO.cleanup()
+
+
+atexit.register(cleanExit)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUZZER, GPIO.OUT)
+
+buzzPwm = GPIO.PWM(BUZZER, 400)
+buzzPwm.start(0)
+
+while True:
+    for i in range(150, 2000):
+        buzzPwm.ChangeFrequency(i)
+        sleep(0.001)
+    for i in range(2000, 150, -1):
+        buzzPwm.ChangeFrequency(i)
+        sleep(0.001)
