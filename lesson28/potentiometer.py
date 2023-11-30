bfcea4dc5e13dd1db7c554dc7d12abf4a52cc10a
Grant Heath <heathgr@gmail.com>
2023-11-29 20:00:58 -0700

    Fix bug in potentiometer handler.

--- lesson28/potentiometer.py
+++ lesson28/potentiometer.py
@@ -6,13 +6,13 @@ from time import sleep
 class Potentiometer:
     def __init__(self, channel):
         self.__potentiometer = MCP3008(channel)
-        self.__process = Process(self.process)
+        self.__process = Process(target=self.processHandler)
 
     def onChange(self, callback):
         self.__callback = callback
         self.__process.start()
 
-    def process(self):
+    def processHandler(self):
         while True:
             newValue = round(self.__potentiometer.value, 2)
             if newValue != self.__value:
