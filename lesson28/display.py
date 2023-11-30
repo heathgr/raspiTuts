8c10de57cfeb6c8419df6060fd226e5db7899f6e
Grant Heath <heathgr@gmail.com>
2023-11-29 20:53:15 -0700

    Display a message on display if the device is editable.

--- lesson28/display.py
+++ lesson28/display.py
@@ -7,13 +7,15 @@ class Display:
 
     def subscribe(self, store):
         store.subscribe(self.update)
+        self.update(store.state)
 
     def update(self, state):
+        isEditableMessage = "set" if state["isEditable"] else "alarm"
         aboveMessage = "<" if state["triggerLessThan"] else ">"
 
         self.__lcd.message(f"Temp: {state['temp']}", 1)
         self.__lcd.message(
-            f"Alarm {aboveMessage} {state['triggerPoint']}.", 2)
+            f"{isEditableMessage} {aboveMessage} {state['triggerPoint']}.", 2)
 
     def clear(self):
         self.__lcd.clear()
