60d6d7ef7f0f6dd26313108a9971cc6ee9c31ad8
Grant Heath <heathgr@gmail.com>
2023-11-30 15:54:09 -0700

    Using threading in potentiometer handeling instead of multiprocessing.

--- lesson28/potentiometer.py
+++ lesson28/potentiometer.py
@@ -1,19 +1,19 @@
-from multiprocessing import Process
+from threading import Thread
 from gpiozero import MCP3008
 from time import sleep
 
 
 class Potentiometer:
     def __init__(self, channel):
+        self.onChange = None
         self.__potentiometer = MCP3008(channel)
-        self.__process = Process(target=self.processHandler)
+        self.__process = Thread(target=self.processHandler)
         self.__value = None
         self.__process.start()
-        self.onChange = None
 
     def processHandler(self):
         while True:
-            if self.onChange != None:
+            if self.onChange:
                 print("checking pot value")
                 newValue = round(self.__potentiometer.value, 2)
                 if newValue != self.__value:
