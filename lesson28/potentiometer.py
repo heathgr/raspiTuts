7a367ee3e95a50894033cb2ada5d719bde671712
Grant Heath <heathgr@gmail.com>
2023-11-29 21:30:23 -0700

    Hope it works...

--- lesson28/potentiometer.py
+++ lesson28/potentiometer.py
@@ -4,13 +4,12 @@ from time import sleep
 
 
 class Potentiometer:
-    onChange = None
-
     def __init__(self, channel):
         self.__potentiometer = MCP3008(channel)
         self.__process = Process(target=self.processHandler)
         self.__value = None
         self.__process.start()
+        self.onChange = None
 
     def processHandler(self):
         while True:
