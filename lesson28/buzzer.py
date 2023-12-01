e8d0512694b9cedc1fbccdbcb81b05488e2b94cc
Grant Heath <heathgr@gmail.com>
2023-11-30 21:28:41 -0700

    More buzzer handler tests.

--- lesson28/buzzer.py
+++ lesson28/buzzer.py
@@ -8,6 +8,15 @@ class Buzzer:
         self.__buzzer = TonalBuzzer(pin)
         sleep(3)
         self.__buzzer.play(Tone("A4"))
-        sleep(3)
+        sleep(0.5)
+        self.__buzzer.play(Tone("C3"))
+        sleep(0.5)
         self.__buzzer.stop()
+        pass
+
+    def subscribe(self, store):
+        store.subscribe(self.update)
+        self.update(store.state)
+
+    def update(self, state):
         pass
