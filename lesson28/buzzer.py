6191cd03d4bb1502c5988706feb20f2c95c35b7b
Grant Heath <heathgr@gmail.com>
2023-11-30 21:50:49 -0700

    Add even more buzzer tests.

--- lesson28/buzzer.py
+++ lesson28/buzzer.py
@@ -1,18 +1,24 @@
 from gpiozero import TonalBuzzer
 from gpiozero.tones import Tone
+from threading import Thread
 from time import sleep
 
 
 class Buzzer:
     def __init__(self, pin):
         self.__buzzer = TonalBuzzer(pin)
-        sleep(3)
-        self.__buzzer.play(Tone("A4"))
-        sleep(0.5)
-        self.__buzzer.play(Tone("C3"))
-        sleep(0.5)
-        self.__buzzer.stop()
+        self.__process = Thread(target=self.processHandler)
+        self.__process.start()
         pass
+
+    def processHandler(self):
+        while True:
+            self.__buzzer.play(Tone("A4"))
+            sleep(0.3)
+            self.__buzzer.play(Tone("C4"))
+            sleep(0.3)
+            self.__buzzer.stop()
+            sleep(0.5)
 
     def subscribe(self, store):
         store.subscribe(self.update)
