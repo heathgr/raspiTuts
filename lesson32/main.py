a5846e8fd5fe36917806e1c3e166ecf8938fc0ee
Grant Heath <heathgr@gmail.com>
2023-12-16 16:36:44 -0700

    Fix input bug.

--- lesson32/main.py
+++ lesson32/main.py
@@ -4,11 +4,11 @@ from gpiozero import OutputDevice, InputDevice
 from time import sleep
 
 out = OutputDevice(19)
-in = InputDevice(21)
+input = InputDevice(21)
 
 while True:
-  out.on()
-  value = in.value
-  out.off()
-  print(f"test: {value}")
-  sleep(0.2)
+    out.on()
+    value = input.value
+    out.off()
+    print(f"test: {value}")
+    sleep(0.2)
