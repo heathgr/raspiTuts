4c733b4e0ad5c12bd674bbbdd6f71de639426227
Grant Heath <heathgr@gmail.com>
2023-12-16 19:18:05 -0700

    Scanning for key press test.

--- lesson32/main.py
+++ lesson32/main.py
@@ -3,14 +3,29 @@
 from gpiozero import OutputDevice, InputDevice
 from time import sleep
 
-# columns
-columns = OutputDevice(21)
-# rows
-rows = InputDevice(19)
+columns = [
+    OutputDevice(21),
+    OutputDevice(20),
+    OutputDevice(16),
+    OutputDevice(12)
+]
+
+rows = [
+    InputDevice(19),
+    InputDevice(13),
+    InputDevice(6),
+    InputDevice(5)
+]
 
 while True:
-    columns.on()
-    value = rows.value
-    columns.off()
-    print(f"test: {value}")
+    columnValue = None
+    rowValue = None
+
+    for rid, row in enumerate(rows):
+        for cid, column in enumerate(columns):
+            column.on()
+            isPressed = row.value
+            column.off()
+            if isPressed:
+                print(f"row: {rid} column: {cid}")
     sleep(0.2)
