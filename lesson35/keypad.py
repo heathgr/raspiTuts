db4ecd8df365a53a78f27042fd5e19ed94dbb695
Grant Heath <heathgr@gmail.com>
2023-12-27 20:44:31 -0700

    Refine keypad input.

--- lesson35/keypad.py
+++ lesson35/keypad.py
@@ -1,23 +1,10 @@
 from gpiozero import OutputDevice, InputDevice, TonalBuzzer
 from gpiozero.tones import Tone
+from threading import Thread
 from time import sleep
 
 
 class Keypad:
-    columns = [
-        OutputDevice(21),
-        OutputDevice(20),
-        OutputDevice(16),
-        OutputDevice(12)
-    ]
-
-    rows = [
-        InputDevice(19),
-        InputDevice(13),
-        InputDevice(6),
-        InputDevice(5)
-    ]
-
     keypadValues = [
         '1', '2', '3', 'A',
         '4', '5', '6', 'B',
@@ -25,15 +12,19 @@ class Keypad:
         '*', '0', '#', 'D',
     ]
 
-    keypadTones = [
-        Tone(400.0), Tone(420.0), Tone(440.0), Tone(460.0),
-        Tone(480.0), Tone(500.0), Tone(520.0), Tone(540.0),
-        Tone(560.0), Tone(580.0), Tone(600.0), Tone(620.0),
-        Tone(640.0), Tone(660.0), Tone(680.0), Tone(700.0),
-    ]
+    def __init__(self, columPins=[21, 20, 16, 12], rowPins=[19, 13, 6, 5], buzzerPin=26):
+        self.__columns = map(lambda x: OutputDevice(x), columPins)
+        self.__rows = map(lambda x: InputDevice(x), rowPins)
+        self.__buzzer = TonalBuzzer(buzzerPin)
+        self.__monitorProcess = Thread(target=self.monitor)
+        self.__monitorProcess.start()
+        self.onChange = None
 
-    def __init__(self):
-        self.buzzer = TonalBuzzer(26)
+    def monitor(self):
+        while True:
+            keypadInput = self.input
+            if self.onChange:
+                self.onChange(keypadInput)
 
     def input(self):
         keyPressedValue = None
@@ -41,17 +32,17 @@ class Keypad:
         while True:
             wasPressed = 0
 
-            for rid, row in enumerate(self.rows):
-                for cid, column in enumerate(self.columns):
+            for rid, row in enumerate(self.__rows):
+                for cid, column in enumerate(self.__columns):
                     column.on()
                     isPressed = row.value
                     column.off()
                     if isPressed:
                         valueIndex = (rid * 4) + cid
                         keyPressedValue = self.keypadValues[valueIndex]
-                        self.buzzer.play(self.keypadTones[valueIndex])
+                        self.__buzzer.play(Tone(500))
                         wasPressed = 1
             if not wasPressed and keyPressedValue:
-                self.buzzer.stop()
+                self.__buzzer.stop()
                 return keyPressedValue
             sleep(0.01)
--- lesson35/main.py
+++ lesson35/main.py
@@ -11,10 +11,9 @@ LCD.init(0X27, 1)
 LCD.write(0, 0, "Hello")
 LCD.write(0, 1, "World")
 
-while True:
-    keypressed = myKeypad.input()
-    if keypressed == "D":
-        print(f"Input code: {keypadInput}")
-        keypadInput = ""
-    if keypressed != "D":
-        keypadInput += keypressed
+
+def onKeypadInput(value):
+    print(f"key pressed: {value}")
+
+
+myKeypad.onChange = onKeypadInput
