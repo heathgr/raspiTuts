eb50187786aa6c7137bc8a0c537515b21e087abc
Grant Heath <heathgr@gmail.com>
2023-12-31 17:38:37 -0700

    Initial diarm tests.

--- lesson35/display.py
+++ lesson35/display.py
@@ -2,6 +2,15 @@ import LCD1602 as LCD
 from appModes import AppModes
 
 
+def maskInput(keypadInput):
+    displayStr = keypadInput
+
+    for i in range(len(displayStr) - 2):
+        displayStr[i] = "*"
+
+    return displayStr.ljust(16)
+
+
 class Display:
     def __init__(self):
         LCD.init(0X27, 1)
@@ -11,15 +20,23 @@ class Display:
         self.update(store.state)
 
     def update(self, state):
-        if state["appMode"] == AppModes.ARMED:
-            LCD.write(0, 0, "Armed           ")
+        appMode = state["appMode"]
+        isArmed = state["isArmed"]
+        armedStatus = "A" if isArmed else "D"
+
+        if appMode == AppModes.ARMED:
+            LCD.write(0, 0, f"Armed          {armedStatus}")
             return
 
-        if state["appMode"] == AppModes.DISARMED:
-            LCD.write(0, 0, "Disarmed        ")
+        if appMode == AppModes.REQUEST_DISARM:
+            LCD.write(0, 0, f"Enter password:{armedStatus}")
+            LCD.write(0, 1, maskInput(state["keypadInput"]))
+
+        if appMode == AppModes.DISARMED:
+            LCD.write(0, 0, f"Disarmed       {armedStatus}")
             return
 
-        LCD.write(0, 0, "                ")
+        LCD.write(0, 0, f"               {armedStatus}")
 
     def clear(self):
         self.__lcd.clear()
--- lesson35/main.py
+++ lesson35/main.py
@@ -9,6 +9,10 @@ from appModes import AppModes
 appState = Store({
     "appMode": AppModes.DISARMED,
     "keypadInput": "",
+    "password": "5555",
+    "newPassword1": "",
+    "newPassword2": "",
+    "isArmed": False,
 })
 
 myKeypad = Keypad()
@@ -16,13 +20,41 @@ myDisplay = Display()
 
 
 def onKeypadInput(value):
-    if value == "A":
-        appState.update({"appMode": AppModes.ARMED})
-        return
+    appMode = appState["appMode"]
 
-    if value == "B":
-        appState.update({"appMode": AppModes.DISARMED})
+    if value == "A":
+        appState.update({
+            "appMode": AppModes.ARMED,
+            "keypadInput": "",
+            "isArmed": True,
+        })
         return
+    elif value == "B" and appMode != AppModes.REQUEST_DISARM:
+        appState.update({
+            "appMode": AppModes.REQUEST_DISARM,
+            "keypadInput": "",
+        })
+        return
+    elif value == "C" and appMode != AppModes.REQUEST_PASSWORD_CHANGE:
+        appState.update({
+            "appMode": AppModes.REQUEST_PASSWORD_CHANGE,
+            "keypadInput": "",
+        })
+        return
+    # TODO use a regex instead of isnumeric so the password can contain # and * characters
+    elif value.isnumeric():
+        keypadInput = appState["keypadInput"]
+        appMode = appState["appMode"]
+
+        if len(keypadInput) == 4:
+            print(f"veryify password: {keypadInput}")
+            if appMode == AppModes.REQUEST_DISARM:
+                if keypadInput == appState["password"]:
+                    appState.update({
+                        "appMode": AppModes.DISARMED,
+                        "isArmed": False,
+                        "keypadInput": "",
+                    })
 
 
 myDisplay.subscribe(appState)
